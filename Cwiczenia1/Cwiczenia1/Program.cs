7415e79ad31d3e0ce234983e5c68b2ef22c6300d
Tofame <80583148+Tofame@users.noreply.github.com>
2025-03-04 14:54:55 +0100

    Add calculator class

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+
 Console.WriteLine("Hello, World!");
+
+public class Calculator
+{
+    public Calculator() { }
+
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int substract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int divide(int a, int b)
+    {
+        try
+        {
+            return a / b;
+        } catch {
+            Console.WriteLine("Cant divide by 0.");
+            return 0;
+        }
+    }
+
+    public int multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
